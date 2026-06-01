6f22b71002f080c1df5f3026fcbd58b7760e91e6
OvNeptuno <jgaq05012026@gmail.com>
2026-06-01 15:36:04 -0600

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const https = require('https');
+const express = require("express");
+const https = require("https");
+const app = express();
 
-// REEMPLAZA CON LA URL DE TU WEB APP DE GOOGLE SHEETS
+
 const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbyM53UIlZFDkXl1HaNo6NGXSIvjb_Q2zROA0VABP1a0wBzvNs29XrRJbT-u097fTBfS/exec";
 
-console.log("Despertador de Google Sheets iniciado...");
 
-// Bucle que ejecuta la función cada 10,000 milisegundos (10 segundos)
+app.get("/", (req, res) => {
+  res.send("Despertador de Google Sheets corriendo 24/7 de forma correcta.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
+});
+
+
 setInterval(() => {
   https.get(GOOGLE_SHEETS_URL, (res) => {
-    console.log(`[${new Date().toISOString()}] Petición enviada. Código estado: ${res.statusCode}`);
+    console.log(`[${new Date().toISOString()}] Señal enviada a Google Sheets. Status: ${res.statusCode}`);
   }).on('error', (e) => {
-    console.error(`Error en la petición: ${e.message}`);
+    console.error(`Error al conectar con Google Sheets: ${e.message}`);
   });
-}, 10000);
+}, 5000);
